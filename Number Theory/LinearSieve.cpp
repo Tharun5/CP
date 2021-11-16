6475cb6a93d20e0631962b5073be4956b8e49323
Tharun <62890884+Tharun5@users.noreply.github.com>
2021-11-16 21:41:41 +0530

    Create LinearSieve.cpp

---
+++ Number Theory/LinearSieve.cpp
@@ -0,0 +1,23 @@
+/**
+ * Description: Computes all prime numbers less than n in linear time.
+ * Source: https://cp-algorithms.com/algebra/prime-sieve-linear.html
+ * Time: O(n)
+*/
+
+const int N = 10000000;
+vector<int> spf(N+1); //stores smallest prime factor of a number
+vector<int> prm; //contains list of primes
+
+void linearSieve(){
+    for (int i=2; i <= N; ++i) {
+        if (spf[i] == 0) {
+            spf[i] = i;
+            prm.push_back(i);
+        }   
+        for (int j=0; j < (int)prm.size() && prm[j] <= spf[i] && i*prm[j] <= N; ++j) {
+            spf[i * prm[j]] = prm[j];
+        }
+    }
+}
+
+//To check if number is prime, Its spf should equal to number itself
