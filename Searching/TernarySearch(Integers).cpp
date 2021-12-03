5e1e157727b378440f390f6ee1a298a7f2c6449c
Tharun <62890884+Tharun5@users.noreply.github.com>
2021-12-03 20:27:59 +0530

    Update TernarySearch(Integers).cpp

--- Searching/TernarySearch(Integers).cpp
+++ Searching/TernarySearch(Integers).cpp
@@ -1,8 +1,37 @@
 /**
  * Description: Ternary Search for Integers
  * Source: https://www.youtube.com/playlist?list=PL-Jc9J83PIiGiFr2WFAiS9nEssqEoCKC4
- * Problem: https://www.spoj.com/problems/KOPC12A/
+ * Problems: 1.https://www.spoj.com/problems/KOPC12A/
+             2.https://codeforces.com/problemset/problem/1479/A
 */
+
+// Binary Search Implementation of Ternary Search for Problem 2
+
+ll query(ll i){
+    ll val;
+    cout<<"? "<<i<<endl;
+    cout << flush;
+    cin>>val;
+    return val;
+}
+ 
+void solve(){
+    ll n;cin>>n;
+    ll l,r;
+    l=1,r=n;
+    while(l<r){
+        ll m1=(l+r)/2;
+        ll m2=m1+1;
+        if(query(m1)<query(m2)){
+            r=m1;
+        }else l=m2;
+    }
+    cout<<"! "<<l<<endl;
+ 
+}
+
+
+// Ternary Search Implementation for Problem 1
 
 ll n;
 vector<ll> h,c;
