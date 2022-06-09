549a40b2349e193065c6acb66cdf3d52b3eec41b
Tharun <62890884+Tharun5@users.noreply.github.com>
2022-06-09 23:13:03 +0530

    Create CoinChangeNoOfWays.cpp

---
+++ DP/Unbounded Knapsack/CoinChangeNoOfWays.cpp
@@ -0,0 +1,28 @@
+/**
+ * Description: Given an array coins representing coins of different denominations and an integer amount representing a total amount of money.
+                Return the number of combinations that make up that amount.
+                If that amount of money cannot be made up by any combination of the coins, return 0.
+                
+ * Source: https://www.youtube.com/watch?v=F7wqWbqYn9g&list=PL_z_8CaSLPWekqhdCPmFohncHwz8TY2Go&index=9 
+           https://www.codingninjas.com/codestudio/problems/number-of-subsets_3952532
+*/
+
+// Similar to Cnt no of Subset sum but this is Unbounded Knapsack variant
+
+int dp[302][5002];
+
+    int CntCoinChange(vector<int> &arr,int n,int sum){
+        for(int i=0;i<sum+1;i++)dp[0][i]=0;
+        for(int i=0;i<n+1;i++)dp[i][0]=1;
+
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<sum+1;j++){
+                if(arr[i-1]<=j){
+                    dp[i][j]=dp[i][j-arr[i-1]] + dp[i-1][j];
+                }
+                else dp[i][j]=dp[i-1][j];
+            }
+        }    
+
+        return dp[n][sum];
+    }
