1abd86178328c0e8d7d1550b804035ab70e817eb
Tharun <62890884+Tharun5@users.noreply.github.com>
2022-06-21 19:59:00 +0530

    Update LCS.cpp

--- DP/LCS/LCS.cpp
+++ DP/LCS/LCS.cpp
@@ -8,17 +8,17 @@
 // Recursion
 
 int lcs(string x, string y, int m, int n){
-        if(m==0 || n==0){
-            return 0;
-        }
-        
-        if(x[m-1]==y[n-1]){
-            return 1+lcs(x,y,m-1,n-1);
-        }
-        else{
-            return max(lcs(x,y,m,n-1), lcs(x,y,m-1,n));
-        }
+    if(m==0 || n==0){
+        return 0;
     }
+
+    if(x[m-1]==y[n-1]){
+        return 1+lcs(x,y,m-1,n-1);
+    }
+    else{
+        return max(lcs(x,y,m,n-1), lcs(x,y,m-1,n));
+    }
+}
 
 
 //----------------------------------------------------------------------------------------------------------------------------------------------
