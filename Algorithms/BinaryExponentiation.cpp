73fff8693f44346e914152a7f70144485b36f9de
Tharun <62890884+Tharun5@users.noreply.github.com>
2022-07-23 23:39:14 +0530

    Create BinaryExponentiation.cpp

---
+++ Algorithms/BinaryExponentiation.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long long nn=abs(n);
+        double ans=1.0;
+        while(nn){
+            if(nn&1){
+                ans*=x;
+                nn-=1;
+            }else{
+                x=x*x;
+                nn/=2;
+            }
+        }
+        
+        if(n<0)ans=1.0/ans;
+        return ans;
+    }
+};
