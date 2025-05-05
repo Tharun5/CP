9c81f880be78cf71f7346a7247cd63aa3ccedf61
Tharun <62890884+Tharun5@users.noreply.github.com>
2025-05-05 19:57:02 +0530

    Update LCSubstring.cpp

--- DP/LCS/LCSubstring.cpp
+++ DP/LCS/LCSubstring.cpp
@@ -4,37 +4,50 @@
  * Problem: https://www.geeksforgeeks.org/longest-common-substring-dp-29/
 */
 
-int dp[2002][2002];
-    
-int LCSubstring(string x, string y, int m, int n){
-    //Initialization
-    for(int i=0;i<m+1;i++)
-        for(int j=0;j<n+1;j++)
-            if(i==0 || j==0)
-                dp[i][j]=0;
-    
-    int ans=0;
-    for(int i=1;i<m+1;i++){
-        for(int j=1;j<n+1;j++){
-            if(x[i-1]==y[j-1]){
-                dp[i][j]=1+dp[i-1][j-1];
-                ans=max(ans,dp[i][j]);
-            }
-            else{
-                dp[i][j]=0;      // If not equal continous chain will be broken and length will be 0 again from that point
-            }
-        }
+int rec(int i, int j, string &s1, string &s2, int m, int n, int &ans, vector<vector<int>> &dp){
+    if(i==m || j==n) return 0;
+
+    if(dp[i][j]!=-1) return dp[i][j];
+
+    int cur = 0;
+
+    if(s1[i]==s2[j]){
+        cur = 1+rec(i+1, j+1,s1,s2,m,n,ans,dp);
     }
-    return ans;     // Here final answer will not be in dp[m][n] it will be max of all lengths in matrix
+
+    // If not equal continous chain will be broken and length will be 0 again from that point
+
+    ans = max(ans, cur);
+
+    rec(i,j+1,s1,s2,m,n,ans,dp);
+    rec(i+1,j,s1,s2,m,n,ans,dp);
+    
+    return dp[i][j]=cur;
 }
 
 
-void solve(){
-    string s1,s2;
-    cin>>s1>>s2;
-    int m,n;
-    m=s1.length();
-    n=s2.length();
-    int ans=LCSubstring(s1,s2,m,n);
-    cout<<ans<<endl;
+
+int lcs(string &s1, string &s2){
+    
+    int m = s1.length();
+    int n = s2.length();
+
+    int ans = 0;
+
+    // vector<vector<int>> dp(m, vector<int>(n, -1));
+    // rec(0,0,s1,s2,m,n,ans,dp);
+
+    vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+
+    for(int i=m-1;i>=0;i--){
+        for(int j=n-1;j>=0;j--){
+            if(s1[i]==s2[j]){
+                dp[i][j] = 1+dp[i+1][j+1];
+                ans = max(ans, dp[i][j]);
+            }
+        }
+    }
+
+    return ans;
+
 }
